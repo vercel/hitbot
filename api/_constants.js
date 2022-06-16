223157ca2e78055da27a31fe10abfae72cd8a199
Adrian Bettridge-Wiese <adrian@op74.net>
2022-06-16 13:26:53 -0500

    Revert "Remove all redis"
    This reverts commit 0a9d95f255cbbb11009daed57e2182921b89d5ee.

--- api/_constants.js
+++ api/_constants.js
@@ -1,2 +1,12 @@
-export const token = process.env.SLACK_BOT_TOKEN;
-export const signingSecret = process.env.SLACK_SIGNING_SECRET;
+import { Redis } from '@upstash/redis'
+
+export const token = process.env.SLACK_BOT_TOKEN
+export const signingSecret = process.env.SLACK_SIGNING_SECRET
+
+const redisURL = process.env.UPSTASH_REDIS_REST_URL
+const redisToken = process.env.UPSTASH_REDIS_REST_TOKEN
+
+export const redis = new Redis({
+  url: redisURL,
+  token: redisToken,
+})
